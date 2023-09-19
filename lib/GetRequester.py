fd9b70f38a3c9bf0ed5bba9dbe5d002b2d206c2a
omarfarouq ahmed <omarfarouq.ahmed@student.moringaschool.com>
2023-09-19 14:39:52 +0300

    DONE

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url =  'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(url)
+        return response.content
+
 
     def load_json(self):
-        pass
+        response_content = self.get_response_body()
+        try:
+            data = json.loads(response_content)
+            return data
+        except json.JSONDecodeError:
+            # Handle JSON decoding error, e.g., by returning None or raising an exception
+            return None
